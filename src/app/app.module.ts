e27db7b4bbf0e1c9535adb4b39fdd2ab5ac955d9
Muhammad Samadov <sam.muhammadali@gmail.com>
2023-03-29 19:19:10 +0500

    localizing to uz-Cyrl

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import {NgModule} from '@angular/core';
+import {DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {ExtraOptions, PreloadAllModules, RouterModule} from '@angular/router';
@@ -15,6 +15,8 @@ import {MatPaginatorIntl} from '@angular/material/paginator';
 import {CustomPaginator} from 'app/custom/CustomPaginatorConfiguration';
 import {FormsModule} from '@angular/forms';
 
+import { IntlModule } from "@progress/kendo-angular-intl";
+import "../../node_modules/@progress/kendo-angular-intl/locales/uz-Cyrl/all.js";
 
 const routerConfig: ExtraOptions = {
     preloadingStrategy: PreloadAllModules,
@@ -42,12 +44,16 @@ const routerConfig: ExtraOptions = {
         LayoutModule,
 
         FormsModule,
+
+        IntlModule,
     ],
     bootstrap: [
         AppComponent
     ],
     providers: [
         {provide: MatPaginatorIntl, useValue: CustomPaginator()},
+        {provide: LOCALE_ID, useValue: 'uz-Cyrl'},
+        {provide: DEFAULT_CURRENCY_CODE, useValue: 'UZS'},
         {
             provide: "BASE_API_URL",
             useValue: 'http://localhost:8093/'
