a43f7d3e32e45914b125a60258a0a138a7ee62d8
BoburbekUsmanov <boburbeku@gmail,.com>
2023-07-23 22:39:53 +0500

    production

--- src/app/modules/production/production/add-modal/add-modal.component.ts
+++ src/app/modules/production/production/add-modal/add-modal.component.ts
@@ -1,10 +1,109 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
+
+interface AutoCompleteCompleteEvent {
+    originalEvent: Event;
+    query: string;
+}
 @Component({
   selector: 'production-add-modal',
   templateUrl: './add-modal.component.html',
-  styleUrls: ['./add-modal.component.scss']
+  styleUrls: ['./add-modal.component.scss'],
 })
-export class AddModalComponent {
+
+
+export class AddModalComponent implements OnInit{
+
+    // Modal
+    visible: boolean = false;
+    showDialog() {
+        this.visible = true;
+    }
+
+    //Filter
+    countries: any[] | undefined;
+
+    filteredCountries: any[] | undefined;
+
+    doc_no: any;
+
+    suggestions: any[] | undefined;
+
+    search(event: AutoCompleteCompleteEvent) {
+        this.suggestions = [...Array(10).keys()].map(item => event.query + '-' + item);
+    };
+
+
+
+    // Calendar
+    Start_at: Date | undefined;
+    Finish_at:Date | undefined;
+
+    // Table's variables
+    products: object[] = [];
+    cols:object[] = [];
+
+    ngOnInit() {
+
+        this.countries = [
+            {
+                "name": "Afghanistan",
+                "code": "AF"
+            },
+            {
+                "name":'Uzbekistan',
+                "code":"UZB",
+            }
+        ]
+
+        this.products = [
+            {
+                id: '1000',
+                code: 'f230fh0g3',
+                name: 'Bamboo Watch',
+                description: 'Product Description',
+                image: 'bamboo-watch.jpg',
+                price: 65,
+                category: 'Accessories',
+                quantity: 24,
+                inventoryStatus: 'INSTOCK',
+                rating: 5
+            },
+        ]
+        this.cols = [
+            { field: 'mebel', header: 'Mebel' },
+            { field: 'buyurtma', header: 'Buyurtmaning Izohi' },
+            { field: 'rang', header: 'Rang,Derevo' },
+            { field: 'soni', header: 'Soni' },
+            { field: 'nechta', header: 'Nechta chiqaramiz' },
+            { field: 'message', header: 'izoh' },
+        ]
+
+
+
+    }
+
+    // Button - Qo'shish
+    addElements() {
+        this.visible = true;
+    }
+
+
+    filterCountry(event: AutoCompleteCompleteEvent) {
+        let filtered: any[] = [];
+        let query = event.query;
+
+        for (let i = 0; i < (this.countries as any[]).length; i++) {
+            let country = (this.countries as any[])[i];
+            if (country.name.toLowerCase().indexOf(query.toLowerCase()) == 0) {
+                filtered.push(country);
+            }
+        }
+
+        this.filteredCountries = filtered;
+    }
+
 
 }
+
+
--- src/app/modules/production/production/production.module.ts
+++ src/app/modules/production/production/production.module.ts
@@ -3,18 +3,33 @@ import {CommonModule} from '@angular/common';
 import {ProductionComponent} from './production.component';
 import {RouterModule} from "@angular/router";
 import {productionRoutes} from "./production.routes";
-import { AddModalComponent } from './add-modal/add-modal.component';
-import { ButtonModule } from 'primeng/button';
+import {AddModalComponent} from './add-modal/add-modal.component';
+import {ButtonModule} from 'primeng/button';
+import {DialogModule} from 'primeng/dialog';
+import {TableModule} from 'primeng/table';
+import {InputTextModule} from 'primeng/inputtext';
+import {SharedModule} from "../../../shared/shared.module";
+import { CalendarModule } from 'primeng/calendar';
+import { AutoCompleteModule } from 'primeng/autocomplete';
+import { TableComponent } from './table/table.component';
+
 
 @NgModule({
     declarations: [
         ProductionComponent,
-        AddModalComponent
+        AddModalComponent,
+        TableComponent
     ],
     imports: [
         RouterModule.forChild(productionRoutes),
         CommonModule,
-        ButtonModule
+        ButtonModule,
+        DialogModule,
+        TableModule,
+        InputTextModule,
+        SharedModule,
+        CalendarModule,
+        AutoCompleteModule
     ]
 })
 export class ProductionModule {
