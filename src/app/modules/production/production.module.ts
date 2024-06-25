47e34a54df043169e2d1128cf2213c2f6268e7ae
Alimov Shaxzod <alimovshaxzod89@gmail.com>
2024-06-25 15:19:14 +0500

    tree added

--- src/app/modules/production/production.module.ts
+++ src/app/modules/production/production.module.ts
@@ -14,6 +14,7 @@ import {MatSelectModule} from "@angular/material/select";
 import {SharedModule} from 'app/shared/shared.module';
 import {MatAutocompleteModule} from "@angular/material/autocomplete";
 import {NgxMatSelectSearchModule} from "ngx-mat-select-search";
+import { TreeSelectModule } from 'primeng/treeselect';
 
 import {productionRoutes} from "./production.routing";
 import {MapComponent} from './map/map.component';
@@ -76,6 +77,7 @@ import {MapModalComponent} from './map/map-modal/map-modal.component';
         SharedModule,
         MatAutocompleteModule,
         NgxMatSelectSearchModule,
+        TreeSelectModule,
 
         ButtonModule,
         TableModule,
